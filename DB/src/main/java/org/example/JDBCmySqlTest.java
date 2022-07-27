db321fb441f762fc20d71f7553fc69d1275fb9a3
SergeJusJ <sergeusj@gmail.com>
2022-07-27 21:19:10 +0300

    testing JDBC

--- DB/src/main/java/org/example/JDBCmySqlTest.java
+++ DB/src/main/java/org/example/JDBCmySqlTest.java
@@ -1,4 +1,6 @@
 package org.example;
+import lt.vcs.jdbc.Customer;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -7,21 +9,20 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Properties;
 
 public class JDBCmySqlTest {
 
 
     public static void main(String args[]) {
-        String url = "jdbc:mysql://localhost:3306/world?characterEncoding=utf8";
+        String url = "jdbc:mysql://localhost:3306/classicmodels?characterEncoding=utf8";
         String user = "root";
-        Connection con;
+        Connection con = null;
         Statement stmt;
         ResultSet rs;
 
-
-
-        String query = "SELECT * FROM classicmodels.orders LIMIT 0,10;";
+        String query = "SELECT * FROM customers";
 
         try {
 
@@ -29,9 +30,7 @@ public class JDBCmySqlTest {
             FileInputStream in = new FileInputStream("C:\\Users\\MARTIN\\IdeaProjects\\Exceptions\\DB\\src\\main\\resources\\db.properties");
             props.load(in);
             String password = props.getProperty("db.password") ;
-
-
-            // opening database connection to MySQL server
+         // opening database connection to MySQL server
             con = DriverManager.getConnection(url, user, password);
 
             // getting Statement object to execute query
@@ -39,12 +38,19 @@ public class JDBCmySqlTest {
 
             // executing SELECT query
             rs = stmt.executeQuery(query);
+            ArrayList<Customer> customers = new ArrayList<>();
 
-           // while (rs.next()) {
-            rs.next();
-                int count = rs.getInt(1);
-                System.out.println("Total number of books in the table : " + count);
-            //}
+            while (rs.next()) {
+           int custNr = rs.getInt("customerNumber");
+                String custName = rs.getString(2);
+                String custCity = rs.getString("city");
+                String custPhoneNr = rs.getString("phone");
+                //System.out.println("Customers data  : " + count);
+               Customer customer = new Customer(custNr,custName,custPhoneNr,custCity);
+                customers.add(customer);
+            }
+            con.close();
+            customers.forEach(System.out::println);
 
         } catch (SQLException sqlEx) {
             sqlEx.printStackTrace();
@@ -53,6 +59,7 @@ public class JDBCmySqlTest {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
     }
 
 
